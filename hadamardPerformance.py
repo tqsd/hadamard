f5cd939257c2e8a115bbdd21a81fa1282a193fd1
JanisCGN <45869180+JanisCGN@users.noreply.github.com>
2022-08-29 17:33:26 +0200

    added MIT license information to hadamardPerformance.py

--- hadamardPerformance.py
+++ hadamardPerformance.py
@@ -1,3 +1,10 @@
+#The following license applies:
+##MIT License
+##Copyright (c) [2022] [Janis Nötzel and Matteo Rosati]
+##Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+##The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+##THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 # For each realization phi_1, ..., phi_K of random phases the Hadamard receiver gets coherent state inputs.
 # Thus it produces coherent state outputs.
 # Given any receiver, the probability of deciding for coherent state b given coherent state a is p_R(b|a).
